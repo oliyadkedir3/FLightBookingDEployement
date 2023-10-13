03d43e837f53eaf51b1f008901cf1d47639721e8
Oliyad Kedir <125308179+oliyadkedir3@users.noreply.github.com>
2023-10-13 16:54:26 +0300

    Changed index.css to style.css

--- api/animatedLogin.php
+++ api/animatedLogin.php
@@ -8,7 +8,7 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="style.css">
 
   </head>
   <body>
